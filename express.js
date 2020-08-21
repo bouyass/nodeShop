68db0a3abf50359e4bb8d891f6d88ec467f4af7f
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-08-21 09:57:48 +0200

    server.js - pauffinement du code pour une meilleure gestion d'erreur

--- express.js
+++ express.js
@@ -1,5 +1,23 @@
 const express = require('express');
 
 const app = express();
+app.use((req,res,next)=>{
+   console.log("requete recue")
+    next()
+});
+
+app.use((req,res,next)=>{
+    res.status(201)
+    next()
+});
+
+app.use((req,res,next)=> {
+    res.json({message:"votre serveur fonctionne"});
+    next()
+});
+
+app.use((req,res,next)=> {
+    console.log("la reponse envoyee avec succes")
+});
 
 module.exports = app;
